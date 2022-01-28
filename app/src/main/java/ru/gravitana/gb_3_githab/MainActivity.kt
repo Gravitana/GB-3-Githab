381b0d8dc1605d1450ef844ebf316e14a0d4df8a
Vitaliy <v.a.grahov@ya.ru>
2022-01-28 12:50:56 +0500

    Initial commit

---
+++ app/src/main/java/ru/gravitana/gb_3_githab/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.gravitana.gb_3_githab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
